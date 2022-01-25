1f8daf38785bbf96463560fc4bf7caeef53a0069
Muhammed Mahmood Alam <muhammed.alam.ext@sivantos.com>
2022-01-25 23:49:30 +0530

    added logo

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from '../assets/FerhaLogo_no_wm.png';
 
 const Header = () => {
 
     return (
         <header className="navigation_container">
+            <img src={logo} alt ='logo' className='nav_logo'/>
             <div className='navigation_item navigation_item_selected'>
               Home
             </div>
